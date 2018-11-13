9efcb18c118b66881d9ab46dbbf5ab49d3498cc5
Unknown <gabrieldossantosvargas@gmail.com>
2018-11-12 22:40:01 -0200

    JOGO E MENU

--- SideShooter6-GameLogic.cpp
+++ SideShooter6-GameLogic.cpp
@@ -1,3 +1,4 @@
+
 #include <allegro5\allegro.h>
 #include <allegro5\allegro_primitives.h>
 #include <allegro5\allegro_font.h>
@@ -6,9 +7,8 @@
 #include "objects.h"
 #include<stdio.h>
 #include "functions.h"
+
 //GLOBALS==============================
-
-
 
 int cont=0;
 int numpulos = 0;
@@ -28,7 +28,6 @@ bool keys[10] = {false, false, false, false, false, false, false, false, false, 
 int main(void)
 {
     //primitive variable
-    bool done = false;
     const int jj []= {60,55,45,43,38,31,25,15,10,5};
     const int dange []= {30,30,40,43,38,31,25,15,10,5};
     const int tempo[]= {2,4,6,8,12,13,15,16,17,19};
@@ -37,21 +36,25 @@ int main(void)
     bool isGameOver = false;
     bool moved = false;
     int direcao=0;
+    bool done = false;
+    int pagina =1;
+    int clickInicial =0;
+    int pos_x;
+    int pos_y;
 
 
-    //object variables
-    SpaceShip ship[2];
-    Bullet bullets[NUM_BULLETS];
-    Comet comets[NUM_COMETS];
-    //Sprites sprite;
-
-    //Allegro variables
     ALLEGRO_DISPLAY *display = NULL;
     ALLEGRO_EVENT_QUEUE *event_queue = NULL;
     ALLEGRO_TIMER *timer = NULL;
-    ALLEGRO_FONT *font18 = NULL;
     ALLEGRO_BITMAP *folha_sprite = NULL;
+    ALLEGRO_BITMAP *telaInicial = NULL;
+    ALLEGRO_BITMAP *luvinha = NULL;
 
+
+     //object variables
+    SpaceShip ship[2];
+    Bullet bullets[NUM_BULLETS];
+    Comet comets[NUM_COMETS];
     //Initialization Functions
     if(!al_init())										//initialize Allegro
         return -1;
@@ -64,7 +67,12 @@ int main(void)
     }
     if(!display)										//test display object
         return -1;
-
+    if (!al_install_mouse())
+    {
+        printf("Falha ao inicializar o mouse");
+        al_destroy_display(display);
+        return -1;
+    }
     al_init_primitives_addon();
     al_install_keyboard();
     al_init_font_addon();
@@ -74,13 +82,18 @@ int main(void)
     timer = al_create_timer(1.0 / FPS);
 
     srand(time(NULL));
-    InitShip(ship);
-    InitBullet(bullets, NUM_BULLETS);
-    InitComet(comets, NUM_COMETS);
 
-    font18 = al_load_font("arial.ttf", 18, 0);
-    folha_sprite = al_load_bitmap("sprite.png");
-    if (!folha_sprite)
+    telaInicial = al_load_bitmap("telaInicial.jpg");
+    luvinha = al_load_bitmap("luvinha.png");
+    if (!telaInicial)
+    {
+        printf("Falha ao carregar telainicial");
+        al_destroy_timer(timer);
+        al_destroy_display(display);
+        al_destroy_event_queue(event_queue);
+        return 0;
+    }
+    if (!luvinha)
     {
         printf("Falha ao carregar sprites");
         al_destroy_timer(timer);
@@ -88,10 +101,15 @@ int main(void)
         al_destroy_event_queue(event_queue);
         return 0;
     }
+
+    InitShip(ship);
+    InitBullet(bullets, NUM_BULLETS);
+    InitComet(comets, NUM_COMETS);
+
     al_register_event_source(event_queue, al_get_keyboard_event_source());
     al_register_event_source(event_queue, al_get_timer_event_source(timer));
     al_register_event_source(event_queue, al_get_display_event_source(display));
-
+    al_register_event_source(event_queue, al_get_mouse_event_source());
     al_start_timer(timer);
 
 
@@ -102,8 +120,8 @@ int main(void)
     | |/ _` |/ __/ _ \  |  _ \|  __| |  _ \ / __| |  _ \ / _` | |
     | | (_| | (_| (_) | | |_) | |  | | | | | (__| | |_) | (_| | |
     |_|\__,_|\___\___/  | .__/|_|  |_|_| |_|\___|_| .__/ \__,_|_|
-           )_)       | |                       | |
-                     |_|                       |_|
+              )_)       | |                       | |
+                        |_|                       |_|
     */
     while(!done)
     {
@@ -113,66 +131,67 @@ int main(void)
 
         if(ev.type == ALLEGRO_EVENT_TIMER)
         {
-            //JUMP
-            jump(ship,jj,tempo,0);
-            jump(ship,jj,tempo,1);
-            //GOLPE
-            golpe(ship,1,dange,tempo);
-            golpe(ship,0,dange,tempo);
-
-            hit(ship,dange,tempo,1);
-            hit(ship,dange,tempo,0);
-            // printf("vida 1- %d ||vida 2-%d\n\n",ship[0].lives,ship[1].lives);
             redraw = true;
-            //MOVE
-            if(keys[DOWN])
-                FireBullet(bullets,5,ship);
-            if(keys[D])
-                MoveShipRight(ship,1);
-            if(keys[A])
-                MoveShipLeft(ship,1);
-            if(keys[LEFT])
-                MoveShipLeft(ship,0);
-            if(keys[RIGHT])
-                MoveShipRight(ship,0);
-            if(!isGameOver)
+            switch(pagina)
             {
-                for(int c=0; c<2; c++)
+            case 2:
+                //JUMP
+                jump(ship,jj,tempo,0);
+                jump(ship,jj,tempo,1);
+                //GOLPE
+                golpe(ship,1,dange,tempo);
+                golpe(ship,0,dange,tempo);
+
+                hit(ship,dange,tempo,1);
+                hit(ship,dange,tempo,0);
+                if(keys[DOWN])
+                    FireBullet(bullets,5,ship);
+                if(keys[D])
+                    MoveShipRight(ship,1);
+                if(keys[A])
+                    MoveShipLeft(ship,1);
+                if(keys[LEFT])
+                    MoveShipLeft(ship,0);
+                if(keys[RIGHT])
+                    MoveShipRight(ship,0);
+                if(!isGameOver)
                 {
-                    if((!moved) && (!ship[c].jump))
+                    for(int c=0; c<2; c++)
                     {
-                        if((ship[c].x>=200)&&(ship[c].x<=800))
+                        if((!moved) && (!ship[c].jump))
                         {
-                            if((ship[c].y<=400)||(ship[c].y>=410))
+                            if((ship[c].x>=200)&&(ship[c].x<=800))
+                            {
+                                if((ship[c].y<=400)||(ship[c].y>=410))
+                                {
+                                    gravity(ship,c);
+                                }
+                            }
+                            else
                             {
                                 gravity(ship,c);
                             }
                         }
-                        else
-                        {
-                            gravity(ship,c);
-                        }
                     }
-                }
-                UpdateBullet(bullets, NUM_BULLETS);
-                //al_draw_filled_rectangle(200, 400, 800, 500, al_map_rgb(255, 0, 0));
-                //StartComet(comets, NUM_COMETS);
-                //UpdateComet(comets, NUM_COMETS);
-                CollideBullet(bullets, NUM_BULLETS, comets, NUM_COMETS, ship);
-                //CollideComet(comets, NUM_COMETS, ship);
+                    UpdateBullet(bullets, NUM_BULLETS);
+                    CollideBullet(bullets, NUM_BULLETS, comets, NUM_COMETS, ship);
 
-                for(int c=0; c<2; c++)
-                {
-                    if(ship[c].lives <= c)
+
+                    for(int c=0; c<2; c++)
                     {
-                        ship[c].y = HEIGHT/4;
-                        ship[c].x = WIDTH/2;
-                        ship[c].lives = 3;
+                        if(ship[c].lives <= c)
+                        {
+                            ship[c].y = HEIGHT/4;
+                            ship[c].x = WIDTH/2;
+                            ship[c].lives = 3;
+                        }
+
                     }
-
                 }
-            }
+                break;
 
+
+            }
         }
         else if(ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
         {
@@ -180,7 +199,10 @@ int main(void)
         }
         else if(ev.type == ALLEGRO_EVENT_KEY_DOWN)
         {
-            switch(ev.keyboard.keycode)
+            switch (pagina)
+            {
+            case 2:
+                switch(ev.keyboard.keycode)
             {
             case ALLEGRO_KEY_ESCAPE:
                 done = true;
@@ -241,11 +263,16 @@ int main(void)
 
                 break;
             }
-        }
-        else if(ev.type == ALLEGRO_EVENT_KEY_UP)
-        {
-            switch(ev.keyboard.keycode)
+                break;
+
+
+            }
+
+        }else if(ev.type == ALLEGRO_EVENT_KEY_UP){
+            switch (pagina)
             {
+                case 2:
+                    switch(ev.keyboard.keycode){
             case ALLEGRO_KEY_ESCAPE:
                 done = true;
                 break;
@@ -286,12 +313,99 @@ int main(void)
                 keys[ENTER] = false;
                 break;
             }
+                    break;
+            }
+        }
+        else if(ev.type == ALLEGRO_EVENT_MOUSE_AXES)
+        {
+            pos_x = ev.mouse.x;
+            pos_y = ev.mouse.y;
+            clickInicial = 0;
+            switch (pagina)
+            {
+            case 1:
+                if((pos_x>=660&&pos_x<=1050)&&(pos_y>=205 && pos_y<=306))
+                {
+                    clickInicial = 1;
+                }
+                else if((pos_x>=660&&pos_x<=1050)&&(pos_y>=330 && pos_y<=412))
+                {
+                    clickInicial = 2;
+                }
+                else if((pos_x>=660&&pos_x<=1050)&&(pos_y>=450 && pos_y<=540))
+                {
+                    clickInicial = 3;
+                }
+                else if((pos_x>=660&&pos_x<=1050)&&(pos_y>=570 && pos_y<=660))
+                {
+                    clickInicial = 4;
+                }
+                else
+                {
+                    clickInicial = 0;
+                }
+                break;
+
+            }
+
+
+        }
+        else if(ev.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN)
+        {
+            switch (pagina)
+            {
+            case 1:
+                switch (clickInicial)
+                {
+                case 1:
+                    printf("começo\n");
+                    pagina = 2;
+                    break;
+                case 2:
+
+                    printf("como jogar\n");
+                    break;
+                case 3:
+
+                    printf("credit\n");
+                    break;
+                case 4:
+
+                    printf("sair\n");
+                    break;
+
+                }
+                break;
+
+            }
         }
 
-        if(redraw && al_is_event_queue_empty(event_queue))
+        if((redraw && al_is_event_queue_empty(event_queue)) && (pagina==1))
         {
-            redraw = false;
+            al_draw_bitmap(telaInicial,0,0,0);
+            switch (clickInicial)
+            {
+            case 1:
+                al_draw_bitmap(luvinha,520,230,0);
 
+                break;
+            case 2:
+                al_draw_bitmap(luvinha,520,360,0);
+
+                break;
+            case 3:
+                al_draw_bitmap(luvinha,520,470,0);
+
+                break;
+            case 4:
+                al_draw_bitmap(luvinha,520,600,0);
+
+                break;
+
+            }
+        }
+        if((redraw && al_is_event_queue_empty(event_queue)) && (pagina==2))
+        {
             if(!isGameOver)
             {
                 for(int c=0; c<2; c++)
@@ -305,30 +419,25 @@ int main(void)
                         }
                     }
                 }
-
-                //DrawComet(comets, NUM_COMETS);
                 al_draw_filled_rectangle(200, 400, 800, 500, al_map_rgb(255, 0, 0));
-
-                // DrawBullet(bullets, NUM_BULLETS);
-                al_draw_bitmap_region(folha_sprite,0,0,170,280,ship[0].x-60,ship[0].y-260,0);
+                //al_draw_bitmap_region(folha_sprite,0,0,170,280,ship[0].x-60,ship[0].y-260,0);
                 DrawShip(ship);
-
-                // al_draw_textf(font18, al_map_rgb(255, 0, 255), 5, 5, 0, "Player has %i lives left. Player has destroyed %i objects", ship.lives, ship.score);
-            }
-            else
-            {
-                //al_draw_textf(font18, al_map_rgb(0, 255, 255), WIDTH / 2, HEIGHT / 2, ALLEGRO_ALIGN_CENTRE, "Game Over. Final Score: %i", ship.score);
-            }
-
-            al_flip_display();
-            al_clear_to_color(al_map_rgb(255,255,255));
         }
-    }
+        }
 
+
+
+        al_flip_display();
+        al_clear_to_color(al_map_rgb(255,255,255));
+
+    }
     al_destroy_event_queue(event_queue);
     al_destroy_timer(timer);
-    al_destroy_font(font18);
+    al_destroy_bitmap(telaInicial);
+    al_destroy_bitmap(luvinha);
     al_destroy_display(display);						//destroy our display object
 
     return 0;
 }
+
+
